3a836608df99b73b4b7d4c8e384d55d8686de1f7
goldhanwool <70783496+goldhanwool@users.noreply.github.com>
2022-04-08 00:42:07 +0900

    'state연습'

--- src/App.js
+++ src/App.js
@@ -9,25 +9,52 @@ class App extends React.Component {
     super(props);
     this.state = {
       list: ['영화관가기', '매일책읽기', '수영배우기'],
-    }
+    };
+    this.text = React.createRef();
   } // constructor
+
+  componentDidMount() {
+    console.log(this.text);
+    console.log(this.text.current);
+  }
 
   render() {
     console.log(this.state)
     return (   
       <div className="App">
-        <div className="container">
-          <hr className="line"/>  
-          <h1 className="title">내 테스트</h1>
+        <Container>
+          <Line/>  
+        <Title>내 테스트</Title>
         {/* 컴포넌트를 넣어줍니다. */}
-        <Test list={this.state.list}/>
-        </div>
+        <Title><Test list={this.state.list}/></Title>
+        </Container>
+      <div>
+        <input type="text" ref={this.text}/>
+      </div>
+
       </div>
     );
-  } //render
+  } // render
 } // class
 
+const Container = styled.div`
+  max-width: 350px;
+  min-height: 80vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 0 auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
 
+const Title = styled.div`
+  color: slateblue;
+  text-align: center;
+`; 
 
+const Line = styled.hr`
+  margin: 16px 0px;
+  text-align: 1px solid #ddd;
+`; 
 
 export default App;
--- src/Test.js
+++ src/Test.js
@@ -6,30 +6,31 @@ const Test =  (props) => {
     const my_list = props.list;
     console.log(my_list)
     return (
-        <div className="lists"> 
+        <ListStyle> 
         { //자바스크립트 시작
             my_list.map((list, index) => {
                 console.log(index);
                 return(<div className='list-item' key={index}>{list}</div>)
             })
         }     
-        </div>
+        </ListStyle>
     )
 }
 
 const ListStyle = styled.div`
-display: flex;
-flex-direction: column;
-height: 100%;
-overflow-x: hidden;
-overflow-y: auto;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    overflow-x: hidden;
+    overflow-y: auto;
+    margin-top: 50px
 `;
 
-const ItemStyle = styled.div`
-padding: 16px;
-margin: 8px;
-background-color: aliceblue;
-`;
+// const ItemStyle = styled.div`
+// padding: 16px;
+// margin: 8px;
+// background-color: aliceblue;
+// `;
 
 
 export default Test;
